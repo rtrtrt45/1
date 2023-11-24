d4140b2ac63ffad3a43852e3c8691cb03c42ef25
Dima <dima_filipiev@bk.ru>
2023-11-24 18:14:26 +0500

    secound commit

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     print('Again')
     print('UFO came and added this line')
     my_superfunction()
+    secound_function()
+
+def secound_function():
+    print('My secound git program')
 
 
 if __name__ == '__main__':
